3598552a5a4376bb6e003ed995ad84e6a03dc632
ZackAMC <andresmila01@gmail.com>
2024-09-17 10:59:34 -0500

    agregamos AUTH de next.js

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,10 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string({
@@ -109,6 +113,8 @@ export async function updateInvoice(
     redirect('/dashboard/invoices');
   }
 
+// Eliminacion de invoice
+
 export async function deleteInvoice(id: string) {
     // throw new Error('Failed to Delete Invoice');
     try {
@@ -120,4 +126,25 @@ export async function deleteInvoice(id: string) {
             message: 'Database Error: Failed to Delete Invoice.',
         }
     }
+}
+
+// Autenticacion de ususario
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
